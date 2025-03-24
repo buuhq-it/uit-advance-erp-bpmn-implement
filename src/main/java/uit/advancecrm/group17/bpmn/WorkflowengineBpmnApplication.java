fe6f40f431444f317f11148b4c2e7565747e495b
BuuHQ-Dev <buuhq.dev@gmail.com>
2025-03-24 11:34:44 +0700

    1st

---
+++ src/main/java/uit/advancecrm/group17/bpmn/WorkflowengineBpmnApplication.java
@@ -0,0 +1,13 @@
+package uit.advancecrm.group17.bpmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowengineBpmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowengineBpmnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uit/advancecrm/group17/bpmn/WorkflowengineBpmnApplicationTests.java
@@ -0,0 +1,13 @@
+package uit.advancecrm.group17.bpmn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowengineBpmnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
